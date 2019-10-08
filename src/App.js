8703a53deb1350035f47f029667f2a69aeedf56f
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-08 15:37:59 +0100

    Refactored reducers to separate folder

--- src/App.js
+++ src/App.js
@@ -4,71 +4,14 @@ import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
 import TodosContext from './contexts/TodosContext';
 
-//const todo_list = (JSON.parse(localStorage.getItem('todo_list'))).todos || [];
-const ADD_TODO = 'ADD_TODO';
-const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE';
-const CLEAR_COMPLETE = 'CLEAR_COMPLETE';
-const ON_INPUT_CHANGE = 'ON_INPUT_CHANGE';
-function reducer(state, action) {
-	let finalState = state;
-	switch (action.type) {
-		case ADD_TODO:
-			finalState = {
-				...state,
-				todos: [
-					...state.todos,
-					{
-						id: Date.now(),
-						item: state.todo,
-						due: state.todoDue,
-						completed: false,
-						tags: state.todoTags ?
-							state.todoTags.toLowerCase().replace(/[^a-z,]/g, '').split(',')
-							: []
-					}
-				],
-				todo: ""
-			}
-			break;
-		case TOGGLE_COMPLETE:
-			finalState = {
-				...state,
-				todos: state.todos.map(todo => {
-					if (todo.id !== action.payload) return todo;
-					return { ...todo, completed: !todo.completed }
-				})
-			};
-			break;
-		case CLEAR_COMPLETE:
-			finalState = {
-				...state,
-				todos: state.todos.filter(todo => !todo.completed)
-			};
-			break;
-		case ON_INPUT_CHANGE:
-			finalState = {
-				...state,
-				...action.payload
-			};
-			break;
-		default:
-			finalState = state;
-			break;
-	}
-	localStorage.setItem('app_todos_state', JSON.stringify(finalState));
-	return finalState;
-}
-
-const initialState = () => {
-	const storage = localStorage.getItem('app_todos_state');
-	return storage ? JSON.parse(storage) : {
-		search: "",
-		todo: "",
-		todoDue: "",
-		todoTags: "",
-		todos: [],
-	}
-}
+import {
+	ADD_TODO,
+	TOGGLE_COMPLETE,
+	CLEAR_COMPLETE,
+	ON_INPUT_CHANGE,
+	reducer,
+	initialState
+} from './reducers';
 
 export default function App(props) {
 	const [state, dispatch] = useReducer(reducer, initialState());
